f0429a1af99769a2738e68cba6ef20abffd6ad72
nkouvelas <nko@eworx.gr>
2015-11-22 20:14:55 +0200

    reerer

--- firmware/PlainFFT.h
+++ firmware/PlainFFT.h
@@ -32,7 +32,7 @@ class PlainFFT {
 		// Functions
 		void complexToMagnitude(double *vReal, double *vImag, uint16_t samples);
 		void compute(double *vReal, double *vImag, uint16_t samples, uint8_t dir);
-		double majorPeakFrequency(double *vD, uint16_t samples, double samplingFrequency);
+		String majorPeakFrequency(double *vD, uint16_t samples, double samplingFrequency);
 		double majorPeakIndex(double *vD, uint16_t samples, double samplingFrequency);
 		void printMagnitudes(double *vM, uint16_t samples);
 		uint8_t revision(void);
